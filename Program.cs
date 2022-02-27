46b4a08082d57750f73b2193cde43d1957f29002
Selman HANCI <selman.hanci@ceng.deu.edu.tr>
2022-02-27 11:02:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace WebsiteCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadWebsite();
+        }
+
+        static void DownloadWebsite()
+        {
+        }
+
+        static void ScrapUrl(string url)
+        {
+        }
+
+        static void FetchWebsiteCodes(string url)
+        {
+        }
+
+        static void SaveFileToDisk(string content)
+        {
+        }
+
+        static void CrawlLinks(string content)
+        {
+        }
+
+        static void StoreAssets(string content)
+        {
+        }
+
+        static void CreateFolder(string path)
+        {
+        }
+    }
+}
